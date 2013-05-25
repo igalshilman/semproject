70f4f889f9070c173f402dc493b97342922e7b69
Igal Shilman <igals@wix.com>
2013-05-25 13:53:05 +0300

    minor

--- module/src/log_entry.h
+++ module/src/log_entry.h
@@ -8,13 +8,13 @@
  */
 #include <limits.h>
 #if (((ULONG_MAX) >> 33) > 0)
-	/* 64 bit */
-	typedef unsigned long cputime64_t;
-	typedef unsigned long cputime_t;
+/* 64 bit */
+typedef unsigned long cputime64_t;
+typedef unsigned long cputime_t;
 #else
-	/* 32 bit */
-	typedef unsigned long long cputime64_t;
-	typedef unsigned long cputime_t;
+/* 32 bit */
+typedef unsigned long long cputime64_t;
+typedef unsigned long cputime_t;
 #endif
 
 /* TASK_COMM_LEN is define at sched.h and it had stayed constat for years. */
@@ -29,43 +29,43 @@
 #endif /* __KERNEL__ */
 
 struct log_entry {
-	unsigned long	timestamp;
-	/* identity - defined at sched.h */
-	char		comm[TASK_COMM_LEN];
-	pid_t		pid;
-	/* Machine learning - types defined at compute_cognitiv_score */
-	long		target;
-	int		output;
-	/* acc stuff defined at sched.h in struct task_struct */
-	cputime_t	user; 
-	cputime_t	kern;
-	/* fuzzy	*/
-	unsigned int	fuzzy_cpu_load_u; 
+  unsigned long	timestamp;
+  /* identity - defined at sched.h */
+  char		comm[TASK_COMM_LEN];
+  pid_t		pid;
+  /* Machine learning - types defined at compute_cognitiv_score */
+  long		target;
+  int		output;
+  /* acc stuff defined at sched.h in struct task_struct */
+  cputime_t	user; 
+  cputime_t	kern;
+  /* fuzzy	*/
+  unsigned int	fuzzy_cpu_load_u; 
   unsigned int	fuzzy_cpu_load_k;
   unsigned int	fuzzy_io_read;
   unsigned int	fuzzy_io_write;
-	unsigned int	fuzzy_net_read;
-	unsigned int	fuzzy_net_write;
-	unsigned int	fuzzy_minor_fault;
-	unsigned int	fuzzy_major_fault;
-	unsigned int 	fuzzy_rss_size;
-	/* raw data */	
+  unsigned int	fuzzy_net_read;
+  unsigned int	fuzzy_net_write;
+  unsigned int	fuzzy_minor_fault;
+  unsigned int	fuzzy_major_fault;
+  unsigned int 	fuzzy_rss_size;
+  /* raw data */	
   unsigned long	io_write; 
-	unsigned long	io_read;
+  unsigned long	io_read;
   unsigned long	net_write;
-	unsigned long	net_read;
-	unsigned long	number_vulentary_context_switches;
+  unsigned long	net_read;
+  unsigned long	number_vulentary_context_switches;
   unsigned long	number_in_vulentary_context_switches;
-	/* memory	*/	
-	unsigned long	minor_fault;
-	unsigned long	major_fault;
-	unsigned long	rss_size;
-	/* system raw data */
-	unsigned long	sys_io_write;
+  /* memory	*/	
+  unsigned long	minor_fault;
+  unsigned long	major_fault;
+  unsigned long	rss_size;
+  /* system raw data */
+  unsigned long	sys_io_write;
   unsigned long	sys_io_read;
-	unsigned long	sys_net_read;
-	unsigned long	sys_net_write;
- 	unsigned long	sys_cpu_load;
+  unsigned long	sys_net_read;
+  unsigned long	sys_net_write;
+  unsigned long	sys_cpu_load;
   unsigned long	sys_mem_load;
 };
 
