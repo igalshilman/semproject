c547e5418974cf46bf2e9e6e96409cc3d4158cad
Igal Shilman <igals@wix.com>
2013-05-25 12:28:54 +0300

    Minor

--- module/penalties.h
+++ module/penalties.h
@@ -2,14 +2,13 @@
 #ifndef __PENALTIES_H__
 #define __PENALTIES_H__
 
-#define PENALTY_RANGE 100 
+#define PENALTY_RANGE (101)
 #define MS_TO_NS(TIME)	((TIME) * 1000 * 1000)	
 
 
 /* 
- * Here we defined the wakeup penalty for a task, as function of its fuzzy membership degreee.
- * a penalty ranges from 6ms (membership degree of 0.) to 43ms (membership degree of 1).
- *
+ * Here we define the wakeup penalty for a task, as function of its fuzzy membership degreee.
+ * A penalty ranges from 6ms (degree of 0) to ~43ms (degree of 1).
  */
 
 static unsigned int task_penalty[PENALTY_RANGE] __read_mostly = {0};
@@ -19,13 +18,13 @@ static void init_task_penalties(void)
 	int i;
 	u64 p = MS_TO_NS(6);
 	
-	for (i = 0; i <= PENALTY_RANGE ; i++) {
-		task_penalty[i] = (int)p;
+	for (i = 0; i < PENALTY_RANGE ; i++) {
+		task_penalty[i] = (unsigned int)p;
 		p = (p * 1020) / 1000;
 	}
 }
 
-static inline int deadline_penalty(int degree)
+static inline unsigned int deadline_penalty(int degree)
 {
 	return task_penalty[degree];
 }
